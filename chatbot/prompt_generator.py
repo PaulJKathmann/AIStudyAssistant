76f59e0b086df6cc3f806f08e8420dd970be52f7
PaulJKathmann <paul.kathmann.98@gmail.com>
2024-01-27 22:46:44 -0500

    removed unused code from templates

--- chatbot/prompt_generator.py
+++ chatbot/prompt_generator.py
@@ -1,25 +1,12 @@
 from chatbot.user import User
 import os
 
-# language proficiency should be measured as grade-level
-# should give defaults so that it will run if no user is given
-# future: maybe better to not pass in the user, but the needed attributes??
-# would be better to store the prompt as a local variable
-
 # Get the directory of the current script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class prompt_generator():
-
     def __init__(self, user: User) -> None:
         self.user = user
-
-    # def read_prompt(self, prompt_name, prompt_type=None):
-    #     folder = "prompts/goals/"
-    #     filepath = os.path.join(BASE_DIR, folder, prompt_name + ".txt")
-    #     with open(filepath, "r") as filename:
-    #         prompt = filename.read()
-    #     return prompt
 
     def generate_prompt(self, topic: str):
         # generate prompt using the obtained details from the database:
