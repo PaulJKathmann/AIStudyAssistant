09616da01822ae638165408d98a7b48442de12ed
cjmaduno <cjmaduno@gmail.com>
2024-01-27 22:03:24 -0500

    login page styling

--- app.py
+++ app.py
@@ -50,8 +50,7 @@ def login_required(test):
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
-
+    return render_template('pages/coursesLandingPage.html')
 
 @app.route('/about')
 def about():
